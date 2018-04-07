6e4f9fe9c8afd09aa9707632a5f515f68e614edb
Alex Siryi <desertkun@gmail.com>
2018-04-07 15:47:30 +0300

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,54 @@
+
+<div align="center">
+<img src="https://cloud.githubusercontent.com/assets/1666014/26267105/0169f088-3cf1-11e7-93e9-2d0d0169eacc.png" width="48">
+</div>
+
+# Hack In
+
+This branch is made for the purpose of development of the anthill itself.
+
+### Mac OS X
+
+To setup minimal development environment on Mac Os X, you will need:
+
+1. Install [PyCharm](https://www.jetbrains.com/pycharm/download) (Community Edition is fine)
+2. Clone this branch, including all submodules
+
+```bash
+git clone -b dev https://github.com/anthill-platform/anthill.git
+cd anthill
+git submodule update --init --recursive
+```
+3. Run installation script
+```bash
+cd dev/osx
+./setup.sh
+```
+
+It would take awhile (up to several hours). This will install Homebrew, and Homebrew will install all of the 
+required components.
+
+4. Open cloned repo in PyCharm. 
+
+5. Setup the Project Interpreter. To do that, go to `Preferences`, select `Project Interpreter`, select `Show All` from the dropdown,
+hit "+", select `Add Local`, and feed it with `/usr/local/venv/dev/bin/python`.
+
+6. Select `all` run configuration, hit Run
+
+<div align="center">
+<img src="https://user-images.githubusercontent.com/1666014/32844921-379a61a6-ca2c-11e7-8640-3a4403a30187.png" height="24">
+</div>
+<br>
+
+7. Open [http://localhost:9500](http://localhost:9500) in your browser
+8. Press "Proceed", login using username `root` and password `anthill`. You should see something like this:
+
+<div align="center">
+<a href="https://user-images.githubusercontent.com/1666014/32834423-3b24fef6-ca0b-11e7-8276-240d3ccb6ce8.png"><img src="https://user-images.githubusercontent.com/1666014/32834374-0ba5b288-ca0b-11e7-8f2a-0d6729f76a60.png" width="250"></a>
+</div>
+
+<br><br>
+<div align="center">
+Happy coding!<br>
+<img src="https://user-images.githubusercontent.com/1666014/32833678-417e7866-ca08-11e7-993c-72eacab0fbee.gif">
+</div>
---
+++ dev/osx/setup.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+
+if [[ $EUID -eq 0 ]]; then
+  echo "Do not run this script as root." 2>&1
+  exit 1
+fi
+
+# install Homebrew first
+export TRAVIS=1
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# bunch of important packages
+brew install git
+brew install mysql
+brew install rabbitmq
+brew install redis
+
+sudo mkdir /usr/local/anthill
+sudo chown -R $(id -u):$(id -g) /usr/local/anthill
+
+# setup a virtualenv
+sudo easy_install virtualenv
+sudo mkdir /usr/local/venv
+sudo chown -R $(id -u):$(id -g) /usr/local/venv
+
+virtualenv /usr/local/venv/dev
+
+# install packages into virtualenv at /usr/local/venv/dev
+
+source /usr/local/venv/dev/bin/activate
+
+/usr/local/venv/dev/bin/pip install termcolor ipaddr ujson pyzmq redis tornado pycrypto mysql-python GitPython
+/usr/local/venv/dev/bin/pip install git+https://github.com/anthill-utils/tornado-redis.git
+/usr/local/venv/dev/bin/pip install git+https://github.com/anthill-utils/PyMySQL.git
+/usr/local/venv/dev/bin/pip install tormysql sphinx
+/usr/local/venv/dev/bin/pip install git+https://github.com/anthill-utils/pyjwt.git
+/usr/local/venv/dev/bin/pip install git+https://github.com/anthill-utils/pika.git
+/usr/local/venv/dev/bin/pip install pyOpenSSL cffi cryptography futures ipgetter expiringdict python-geoip
+/usr/local/venv/dev/bin/pip install python-geoip-geolite2-yplan psutil lazy
+
+git clone https://github.com/anthill-utils/v8py.git
+cd v8py
+export MACOSX_DEPLOYMENT_TARGET=10.8
+CFLAGS='-Wall -O0 -g' /usr/local/venv/dev/bin/python setup.py install
+cd ../
+
+# start mysql server
+ln -sfv /usr/local/opt/mysql/*.plist ~/Library/LaunchAgents
+launchctl load ~/Library/LaunchAgents/homebrew.mxcl.mysql.plist
+
+# create various databases
+mysql -h 127.0.0.1 -u root -e "create database dev_admin;"
+mysql -h 127.0.0.1 -u root -e "create database dev_login;"
+mysql -h 127.0.0.1 -u root -e "create database dev_config;"
+mysql -h 127.0.0.1 -u root -e "create database dev_dlc;"
+mysql -h 127.0.0.1 -u root -e "create database dev_environment;"
+mysql -h 127.0.0.1 -u root -e "create database dev_event;"
+mysql -h 127.0.0.1 -u root -e "create database dev_exec;"
+mysql -h 127.0.0.1 -u root -e "create database dev_game;"
+mysql -h 127.0.0.1 -u root -e "create database dev_leaderboard;"
+mysql -h 127.0.0.1 -u root -e "create database dev_message;"
+mysql -h 127.0.0.1 -u root -e "create database dev_profile;"
+mysql -h 127.0.0.1 -u root -e "create database dev_promo;"
+mysql -h 127.0.0.1 -u root -e "create database dev_social;"
+mysql -h 127.0.0.1 -u root -e "create database dev_store;"
+mysql -h 127.0.0.1 -u root -e "create database dev_static;"
+mysql -h 127.0.0.1 -u root -e "create database dev_report;"
+
+# generate private key pair
+mkdir ../../.anthill-keys
+openssl genrsa -des3 -passout pass:wYrA9O187G71ILmZr67GZG945SgarS4K -out ../../.anthill-keys/anthill.pem 512
+openssl rsa -in ../../.anthill-keys/anthill.pem -passin pass:wYrA9O187G71ILmZr67GZG945SgarS4K -outform PEM -pubout -out ../../.anthill-keys/anthill.pub
+
+# start rest of the services
+brew services start rabbitmq
+brew services start redis
